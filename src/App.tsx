c3140680c1a1931e9bfb099b8e5ca77b1b4ca847
rafaelsoeiro <rafaelssoeiro@gmail.com>
2023-10-13 12:31:56 -0300

    :sparkles: feat: create styled component for title and paragraph and add font link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { StyledTitle, StyledParagraph } from "./styles/typography";
+
 function App() {
   return (
     <div>
-      <h1>inicio</h1>
+      <StyledTitle tag="h1" fontSize="lg" fontWeigth={400}>
+        Pokedashboard
+      </StyledTitle>
+      <StyledParagraph fontSize="md">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum fuga cum,
+        veritatis natus similique, ad illo error possimus tempore consectetur ab
+        modi omnis! Saepe iure consequatur modi sit molestiae voluptas!
+      </StyledParagraph>
     </div>
   );
 }
