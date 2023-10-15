160cf391ec4f6923918846f8c9a0eddeffe9d3b5
rafaelsoeiro <rafaelssoeiro@gmail.com>
2023-10-15 18:20:15 -0300

    :sparkles: feat: create style for header component

---
+++ src/components/Header/style.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  padding: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2rem;
+  .userMenu {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    img {
+      width: 50px;
+      height: 50px;
+      object-fit: cover;
+    }
+  }
+  @media (max-width: 600px) {
+    flex-direction: column-reverse;
+    .userMenu {
+      width: 100%;
+      justify-content: flex-end;
+    }
+  }
+`;
+
+export const StyledHeaderForm = styled.form`
+  display: inline-flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 59px;
+  background: ${({ theme }) => theme.colors.white20};
+  border-radius: 10px;
+  gap: 1rem;
+
+  input {
+    font-family: "Poppins", sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    background: transparent;
+    border: none;
+    width: 100%;
+    max-width: 373px;
+    color: ${({ theme }) => theme.colors.white};
+    ::placeholder {
+      ${({ theme }) => theme.colors.white50};
+    }
+  }
+  @media (max-width: 600px) {
+    input {
+      height: 52px;
+    }
+  }
+`;
