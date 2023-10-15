abf7ea849283892e673f5240d913584751d32fe6
rafaelsoeiro <rafaelssoeiro@gmail.com>
2023-10-15 18:21:11 -0300

    :recycle: fix: fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ColorsTheme, GlobalStyle } from "./styles/globalStyle.tsx";
+import { ColorsTheme, GlobalStyle } from "./styles/globalStyle.ts";
 import { ThemeProvider } from "styled-components";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
